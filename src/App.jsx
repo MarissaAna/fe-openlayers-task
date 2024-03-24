cae2396a5ee9b25f87bfd6a48636e57e2cc84cbb
marissa.ana <marissa.ana07@gmail.com>
2024-03-24 21:40:12 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,33 @@ import OSM from "ol/source/OSM.js";
 import { Draw, Snap } from "ol/interaction.js";
 import { Vector as VectorSource } from "ol/source.js";
 import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
-import "./App.css";
+import { fromLonLat } from "ol/proj.js";
+import {
+  CircleStop,
+  CircleDot,
+  Spline,
+  Hexagon,
+  ZoomOut,
+  ZoomIn,
+} from "lucide-react";
+
+import { defaults as defaultControls } from "ol/control";
+import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style.js";
+import { LineString } from "ol/geom";
+import { Overlay } from "ol";
+import { unByKey } from "ol/Observable";
+import { getVectorContext } from "ol/render";
+import { easeOut } from "ol/easing";
+
+// import {
+//   style,
+//   labelStyle,
+//   tipStyle,
+//   modifyStyle,
+//   segmentStyle,
+// } from "./components/Style";
+// import { getArea, getLength } from "./utils/Measure";
+
 // import { Polygon } from "ol/geom";
 
 // const useMap = () => {}; logic yang dipisah bukan comp
@@ -38,6 +64,10 @@ function App() {
       }
     });
 
+    //
+    const duration = 1000;
+    const source = new VectorSource();
+
     const vector = new VectorLayer({
       source: sourceRef.current,
       style: {
@@ -46,17 +76,54 @@ function App() {
         "stroke-width": 2,
         "circle-radius": 7,
         "circle-fill-color": "#18f400",
+        "icon-src":
+          "https://emoticon.id/wp-content/uploads/2020/05/%F0%9F%94%A5-Api-WhatsApp.png",
+        "icon-width": 40,
+        "icon-height": 40,
       },
     });
 
     const map = new Map({
       layers: [tile, vector],
       view: new View({
-        center: [0, 0],
-        zoom: 2,
+        center: fromLonLat([107.60981, -6.914744]),
+        zoom: 12,
       }),
       target: mapRef.current,
       controls: [],
+    });
+
+    //Animation
+    source.on("addfeature", (e) => {
+      const feature = e.feature;
+      const start = Date.now();
+      const flashGeom = feature.getGeometry().clone();
+      const listenerKey = tile.on("postrender", (event) => {
+        const frameState = event.frameState;
+        const elapsed = frameState.time - start;
+        if (elapsed >= duration) {
+          unByKey(listenerKey);
+          return;
+        }
+        const vectorContext = getVectorContext(event);
+        const elapsedRatio = elapsed / duration;
+        const radius = easeOut(elapsedRatio) * 35 + 5;
+        const opacity = easeOut(1.5 - elapsedRatio);
+
+        const style = new Style({
+          image: new CircleStyle({
+            radius: radius,
+            stroke: new Stroke({
+              color: "rgba(255, 255, 255, " + opacity + ")",
+              width: 0.75 + opacity,
+            }),
+          }),
+        });
+
+        vectorContext.setStyle(style);
+        vectorContext.drawGeometry(flashGeom);
+        mapInstanceRef.current.render();
+      });
     });
 
     mapInstanceRef.current = map;
@@ -66,23 +133,12 @@ function App() {
     };
   }, []);
 
-  const zoomOut = () => {
-    const view = mapInstanceRef.current.getView();
-    const zoom = view.getZoom();
-    view.setZoom(zoom - 1);
-  };
-
-  const zoomIn = () => {
-    const view = mapInstanceRef.current.getView();
-    const zoom = view.getZoom();
-    view.setZoom(zoom + 1);
-  };
-
   const addInteraction = (type) => {
     const draw = new Draw({
       source: sourceRef.current,
       type,
     });
+
     mapInstanceRef.current.addInteraction(draw);
     const snap = new Snap({ source: sourceRef.current });
     mapInstanceRef.current.addInteraction(snap);
@@ -102,53 +158,19 @@ function App() {
     }
   };
 
-  console.log("rerender");
+  // console.log("rerender");
+  /**
+   * Handles zooming in or out of the map.
+   * @param {React.MutableRefObject<Map>} mapRef - Reference to the map instance.
+   * @param {number} value - Zoom level adjustment.
+   */
 
-  // const polygonDrawInteraction = (type) => {
-  //   const map = mapInstanceRef.current;
-  //   if (drawInteraction) {
-  //     map.removeInteraction(drawInteraction);
-  //     setDrawInteraction(null);
-  //     setEditorStatus("");
-  //   } else {
-  //     addInteraction("Polygon");
-  //     const snap = new Snap({ source: sourceRef.current });
-  //     map.addInteraction(snap);
-  //   }
-  // };
-
-  // const multiLineDrawInteraction = () => {
-  //   const map = mapInstanceRef.current;
-  //   if (drawInteraction) {
-  //     map.removeInteraction(drawInteraction);
-  //     setDrawInteraction(null);
-  //   } else {
-  //     const draw = new Draw({
-  //       source: sourceRef.current,
-  //       type: "LineString",
-  //     });
-  //     map.addInteraction(draw);
-  //     const snap = new Snap({ source: sourceRef.current });
-  //     map.addInteraction(snap);
-  //     setDrawInteraction(draw);
-  //   }
-  // };
-  // const polygonDrawInteraction = () => {
-  //   const map = mapInstanceRef.current;
-  //   if (drawInteraction) {
-  //     map.removeInteraction(drawInteraction);
-  //     setDrawInteraction(null);
-  //   } else {
-  //     const draw = new Draw({
-  //       source: sourceRef.current,
-  //       type: "Polygon",
-  //     });
-  //     map.addInteraction(draw);
-  //     const snap = new Snap({ source: sourceRef.current });
-  //     map.addInteraction(snap);
-  //     setDrawInteraction(draw);
-  //   }
-  // };
+  const handleZoom = (mapRef, value) => {
+    const map = mapRef.current;
+    const view = map.getView();
+    const zoom = view.getZoom();
+    view.animate({ zoom: zoom + value, duration: 200 });
+  };
 
   return (
     <>
@@ -156,49 +178,16 @@ function App() {
         <div className="w-full h-screen" ref={mapRef}>
           <div className="">
             <button
-              onClick={zoomIn}
+              onClick={() => handleZoom(mapInstanceRef, +2)}
               className="flex top-0 absolute z-10 bg-button p-1 m-2 rounded-md"
-              id="zoomIn"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-zoom-in"
-              >
-                <circle cx="11" cy="11" r="8" />
-                <line x1="21" x2="16.65" y1="21" y2="16.65" />
-                <line x1="11" x2="11" y1="8" y2="14" />
-                <line x1="8" x2="14" y1="11" y2="11" />
-              </svg>
+              <ZoomIn />
             </button>
             <button
-              onClick={zoomOut}
+              onClick={() => handleZoom(mapInstanceRef, -2)}
               className="flex top-10 absolute z-10 bg-button p-1 m-2  rounded-md font-bold"
-              id="zoomOut"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-zoom-out"
-              >
-                <circle cx="11" cy="11" r="8" />
-                <line x1="21" x2="16.65" y1="21" y2="16.65" />
-                <line x1="8" x2="14" y1="11" y2="11" />
-              </svg>
+              <ZoomOut />
             </button>
             <button
               onClick={() => {
@@ -207,39 +196,7 @@ function App() {
               }}
               className="flex top-30 absolute z-10 bg-button p-1 m-2 rounded-md"
             >
-              {editorStatus === "Point" ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-circle-stop"
-                >
-                  <circle cx="12" cy="12" r="10" />
-                  <rect width="6" height="6" x="9" y="9" />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-circle-dot"
-                >
-                  <circle cx="12" cy="12" r="10" />
-                  <circle cx="12" cy="12" r="1" />
-                </svg>
-              )}
+              {editorStatus === "Point" ? <CircleStop /> : <CircleDot />}
             </button>
             <button
               onClick={() => {
@@ -248,40 +205,7 @@ function App() {
               }}
               className="flex top-40 absolute z-10 bg-button p-1 m-2 rounded-md"
             >
-              {editorStatus === "LineString" ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-circle-stop"
-                >
-                  <circle cx="12" cy="12" r="10" />
-                  <rect width="6" height="6" x="9" y="9" />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-spline"
-                >
-                  <circle cx="19" cy="5" r="2" />
-                  <circle cx="5" cy="19" r="2" />
-                  <path d="M5 17A12 12 0 0 1 17 5" />
-                </svg>
-              )}
+              {editorStatus === "LineString" ? <CircleStop /> : <Spline />}
             </button>
             <button
               onClick={() => {
@@ -290,38 +214,7 @@ function App() {
               }}
               className="flex top-50 absolute z-10 bg-button p-1 m-2 rounded-md"
             >
-              {editorStatus === "Polygon" ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-circle-stop"
-                >
-                  <circle cx="12" cy="12" r="10" />
-                  <rect width="6" height="6" x="9" y="9" />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-hexagon"
-                >
-                  <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />
-                </svg>
-              )}
+              {editorStatus === "Polygon" ? <CircleStop /> : <Hexagon />}
             </button>
           </div>
         </div>
